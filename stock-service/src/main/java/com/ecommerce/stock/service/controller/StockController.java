5f4ee2bdb1727f23b1571806b086348820d8982f
nodirshox <mr.nodirbek77@gmail.com>
2022-10-10 20:56:10 -0500

    feature/stock-api

--- stock-service/src/main/java/com/ecommerce/stock/service/controller/StockController.java
+++ stock-service/src/main/java/com/ecommerce/stock/service/controller/StockController.java
@@ -3,6 +3,7 @@ package com.ecommerce.stock.service.controller;
 import com.ecommerce.stock.service.dto.request.CreateStockRequestDTO;
 import com.ecommerce.stock.service.dto.request.UpdateStockRequestDTO;
 import com.ecommerce.stock.service.dto.response.CreateStockResponseDTO;
+import com.ecommerce.stock.service.dto.response.GetStockResponseDTO;
 import com.ecommerce.stock.service.dto.response.UpdateStockResponseDTO;
 import com.ecommerce.stock.service.service.StockService;
 import lombok.RequiredArgsConstructor;
@@ -23,5 +24,10 @@ public class StockController {
     @PutMapping
     public UpdateStockResponseDTO update(@RequestBody UpdateStockRequestDTO updateStockRequestDTO) {
         return stockService.update(updateStockRequestDTO);
+    }
+
+    @GetMapping("/products/{productId}")
+    public GetStockResponseDTO get(@PathVariable Long productId) {
+        return stockService.get(productId);
     }
 }
--- stock-service/src/main/java/com/ecommerce/stock/service/service/StockService.java
+++ stock-service/src/main/java/com/ecommerce/stock/service/service/StockService.java
@@ -3,9 +3,11 @@ package com.ecommerce.stock.service.service;
 import com.ecommerce.stock.service.dto.request.CreateStockRequestDTO;
 import com.ecommerce.stock.service.dto.request.UpdateStockRequestDTO;
 import com.ecommerce.stock.service.dto.response.CreateStockResponseDTO;
+import com.ecommerce.stock.service.dto.response.GetStockResponseDTO;
 import com.ecommerce.stock.service.dto.response.UpdateStockResponseDTO;
 
 public interface StockService {
     CreateStockResponseDTO create(CreateStockRequestDTO createStockRequestDTO);
     UpdateStockResponseDTO update(UpdateStockRequestDTO updateStockRequestDTO);
+    GetStockResponseDTO get(Long productId);
 }
