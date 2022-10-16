6c447ff54db1f07a3f162a93545d0f52f72a123b
nodirshox <mr.nodirbek77@gmail.com>
2022-10-15 20:45:56 -0500

    custom secrets

--- payment-service/src/main/java/com/ecommerce/payment/service/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/ecommerce/payment/service/service/impl/PaymentServiceImpl.java
@@ -49,8 +49,21 @@ public class PaymentServiceImpl implements PaymentService {
     @Value("${config.order-service-port}")
     private Integer ORDER_SERVICE_PORT;
 
-    @Value("${config.secret-key}")
-    private String SECRET_KEY;
+    @Value("${config.bank-service-key}")
+    private String BANK_SERVICE_SECRET_KEY;
+
+    @Value("${config.credit-card-service-key}")
+    private String CARD_SERVICE_SECRET_KEY;
+
+    @Value("${config.shipment-service-key}")
+    private String SHIPPING_SERVICE_SECRET_KEY;
+
+    @Value("${config.order-service-key}")
+    private String ORDER_SERVICE_SECRET_KEY;
+
+    static enum SERVICES {
+        ORDER_SERVICE, BANK_SERVICE, CREDIT_CARD_SERVICE, SHIPPING_SERVICE
+    }
 
     private final PaymentRepository paymentRepository;
     private final ModelMapper mapper;
@@ -70,7 +83,7 @@ public class PaymentServiceImpl implements PaymentService {
 
         // Checking the order
         log.info("Sending order service method");
-        HttpEntity<Void> orderEntity = new HttpEntity<>(getHeader());
+        HttpEntity<Void> orderEntity = new HttpEntity<>(getHeader(SERVICES.ORDER_SERVICE));
         Response addressResponse = restTemplate.exchange(
                 ORDER_SERVICE + "/order/orders/"+ paymentDTO.getOrderId() +"?prop=address",
                 HttpMethod.GET,
@@ -88,20 +101,21 @@ public class PaymentServiceImpl implements PaymentService {
         mapper.map(addressResponse.getData(),address);
 
         log.info("Sending payment method");
-        HttpEntity<AmountRequestDTO> httpEntity = new HttpEntity<>(amountRequestDTO,getHeader());
         if (payment.getPaymentMethod() == Payment.PaymentMethod.BANK) {
             log.info("Sending payment method to bank service");
+            HttpEntity<AmountRequestDTO> httpEntity = new HttpEntity<>(amountRequestDTO,getHeader(SERVICES.BANK_SERVICE));
             response = restTemplate.postForObject(BANK_SERVICE + "/bank/payments",httpEntity,Response.class);
             log.info("Received response from bank service", response);
 
         } else if (payment.getPaymentMethod() == Payment.PaymentMethod.CC) {
             log.info("Sending payment method to credit card service");
+            HttpEntity<AmountRequestDTO> httpEntity = new HttpEntity<>(amountRequestDTO,getHeader(SERVICES.CREDIT_CARD_SERVICE));
             response = restTemplate.postForObject(CREDIT_CARD_SERVICE + "/card/credit-card/payments",httpEntity,Response.class);
             log.info("Received response from credit card service", response);
         }
 
         log.info("Sending shipping service method");
-        HttpEntity<Address> addressHttpEntity = new HttpEntity<>(address,getHeader());
+        HttpEntity<Address> addressHttpEntity = new HttpEntity<>(address,getHeader(SERVICES.SHIPPING_SERVICE));
         Response shipmentResponse = restTemplate.postForObject(SHIPMENT_SERVICE + "/shipping/shipment", addressHttpEntity,Response.class);
         log.info("Received response from shipping service method");
 
@@ -128,11 +142,21 @@ public class PaymentServiceImpl implements PaymentService {
         Payment payment = paymentRepository.findById(id).orElse(null);
         return new Response(payment,true);
     }
-    private HttpHeaders getHeader() {
+    private HttpHeaders getHeader(SERVICES type) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.set("key",SECRET_KEY);
+
+        if (type == SERVICES.SHIPPING_SERVICE) {
+            headers.set("key", SHIPPING_SERVICE_SECRET_KEY);
+        } else if (type == SERVICES.ORDER_SERVICE) {
+            headers.set("key", ORDER_SERVICE_SECRET_KEY);
+        } else if (type == SERVICES.BANK_SERVICE) {
+            headers.set("key", BANK_SERVICE_SECRET_KEY);
+        } else if (type == SERVICES.CREDIT_CARD_SERVICE) {
+            headers.set("key", CARD_SERVICE_SECRET_KEY);
+        }
+
         return headers;
     }
 }
